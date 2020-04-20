71a433e1a6578c3a42b2762f63f03e4e5196f752
Sergei Pilyugin <42743933+SergeiPil@users.noreply.github.com>
2020-04-21 00:14:05 +0400

    Added WebServerApplication + Frontend Servlet

---
+++ src/main/java/com/javaskills/Frontend.java
@@ -0,0 +1,27 @@
+package com.javaskills;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Frontend extends HttpServlet {
+
+    private String login = "";
+
+    /**
+     * receive GET request
+     */
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    /**
+     * receive POST request
+     */
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+}
