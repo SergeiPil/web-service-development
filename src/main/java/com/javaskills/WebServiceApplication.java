a6e50909080d63b0c9d042e0cf66471e35adf784
Sergei Pilyugin <42743933+SergeiPil@users.noreply.github.com>
2020-04-22 00:23:24 +0400

    Added PageGenerator + Refactoring Servlet

--- src/main/java/com/javaskills/WebServiceApplication.java
+++ src/main/java/com/javaskills/WebServiceApplication.java
@@ -1,17 +1,27 @@
 package com.javaskills;
 
+import com.javaskills.servlets.AllRequestServlets;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class WebServiceApplication {
     public static void main(String[] args) throws Exception {
-        Frontend frontend = new Frontend();
-        Server server = new Server(8080);
-
+        /**
+         * Creation Servlet
+         */
+        AllRequestServlets allRequestServlets = new AllRequestServlets();
+        /**
+         * Creation Handler
+         */
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        /**
+         * Added Servlet to Handler
+         */
+        context.addServlet(new ServletHolder(allRequestServlets), "/*");
+
+        Server server = new Server(8080);
         server.setHandler(context);
-        context.addServlet(new ServletHolder(frontend), "/authform");
 
         server.start();
         server.join();
